1a9ca50deaa2bee595177291a1bb8415adb657fd
Scruner <aoveden1977@mail.ru>
2021-04-20 23:58:29 +0300

    1. Телефонный справочник на базе ArrayList [#515636]

---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,29 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+
+    private ArrayList<Person> persons = new ArrayList<Person>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person searchPerson : persons) {
+            if (searchPerson.getName()
+                    .contains(key)
+                    || searchPerson.getSurname()
+                    .contains(key)
+                    || searchPerson.getPhone()
+                    .contains(key)
+                    || searchPerson.getAddress()
+                    .contains(key)) {
+                result.add(searchPerson);
+            }
+        }
+        return result;
+    }
+}
