7101d995113634ad2c041d966799261ca078bb73
Scruner <aoveden1977@mail.ru>
2021-04-20 15:36:49 +0300

    1.8. Состояние объекта [#515721]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -3,8 +3,20 @@ package ru.job4j.oop;
 public class Battery {
     private int load;
 
+    public Battery(int load) {
+        this.load = load;
+    }
+
     public void exchange(Battery another) {
         this.load = this.load + another.load;
         another.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery battery = new Battery(10);
+        Battery anotherBatt = new Battery(5);
+        System.out.println("battary: " + battery.load + ". anotherBatt: " + anotherBatt.load);
+        battery.exchange(anotherBatt);
+        System.out.println("battary: " + battery.load + ". anotherBatt: " + anotherBatt.load);
     }
 }
