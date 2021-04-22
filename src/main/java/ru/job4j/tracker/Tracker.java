a38373eb6d57719edbfdcfa54710c1a03b1f93b6
Scruner <aoveden1977@mail.ru>
2021-04-22 14:32:41 +0300

    7. Метод удаления заявки Tracker.delete [#515739]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -58,17 +58,17 @@ public class Tracker {
         return false;
     }
 
-//    public boolean delete(int id) {
-//        int index = indexOf(id);
-//        if (index != -1) {
-//            int start = index + 1;
-//            int distPos = index;
-//            int length = size - index;
-//            System.arraycopy(items, start, items, distPos, length);
-//            items[size - 1] = null;
-//            size--;
-//            return true;
-//        }
-//        return false;
-//    }
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        if (index != -1) {
+            int start = index + 1;
+            int distPos = index;
+            int length = size - index;
+            System.arraycopy(items, start, items, distPos, length);
+            items[size - 1] = null;
+            size--;
+            return true;
+        }
+        return false;
+    }
 }
