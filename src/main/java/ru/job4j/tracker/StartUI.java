f52c0ea0de6bcf28acf83ecbe08524edd84d51bf
Scruner <aoveden1977@mail.ru>
2021-05-01 18:44:34 +0300

    2. Singleton [#515700]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -39,5 +39,8 @@ public class StartUI {
                 new ExitAction(output)
         };
         new StartUI(output).init(input, tracker, actions);
+        Log4File log = Log4File.getInstance();
+        log.add("add new Item");
+        log.save();
     }
 }
