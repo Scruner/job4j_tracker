8d763b332dc8dcb6a4226a9f6f2db920e773c8b9
Scruner <aoveden1977@mail.ru>
2021-05-06 22:55:22 +0300

    1. Уникальные задачи. [#515649]

---
+++ src/main/java/ru/job4j/collection/FullSearch.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class FullSearch {
+
+    public static HashSet<String> extractNumber(List<Task> list) {
+        HashSet<String> numbers = new HashSet<>();
+        for (String num : numbers) {
+            numbers.add(num);
+        }
+        return numbers;
+    }
+}
