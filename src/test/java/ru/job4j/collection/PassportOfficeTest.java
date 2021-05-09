1108e87befda298cf2134ab9a37c9ecee6bc0ffa
Scruner <aoveden1977@mail.ru>
2021-05-09 14:33:54 +0300

    1. Паспорт и жители [#515645]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -22,6 +22,6 @@ public class PassportOfficeTest {
         PassportOffice office = new PassportOffice();
         office.add(citizen);
         office.add(citizen1);
-        assertThat(false, is(false));
+        assertFalse(office.add(citizen1));
     }
 }
