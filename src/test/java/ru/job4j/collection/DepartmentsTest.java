2a770dc06fd7eff708ccd9f16d9044bb75c5bf0c
Scruner <aoveden1977@mail.ru>
2021-05-27 11:32:42 +0300

    2. Отсортировать департаменты [#34352 #160139]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -28,21 +28,53 @@ public class DepartmentsTest {
 
     @Test
     public void whenSortAsc() {
-        List<String> input = Arrays.asList("K1/SK1","K1/SK1/SSK1", "K1/SK1/SSK2",  "K1/SK2"
-                , "K2", "K2/SK1/SSK1", "K2/SK1/SSK2");
-        List<String> expect = Arrays.asList("K1", "K1/SK1", "K1/SK1/SSK1", "K1/SK1/SSK2", "K1/SK2"
-                , "K2", "K2/SK1", "K2/SK1/SSK1", "K2/SK1/SSK2");
+        List<String> input = Departments.fillGaps(Arrays.asList(
+                "K1/SK1"
+                , "K1/SK1/SSK1"
+                , "K1/SK1/SSK2"
+                , "K1/SK2"
+                , "K2"
+                , "K2/SK1/SSK1"
+                , "K2/SK1/SSK2")
+        );
+        List<String> expect = Arrays.asList(
+                "K1"
+                , "K1/SK1"
+                , "K1/SK1/SSK1"
+                , "K1/SK1/SSK2"
+                , "K1/SK2"
+                , "K2"
+                , "K2/SK1"
+                , "K2/SK1/SSK1"
+                , "K2/SK1/SSK2"
+        );
         Departments.sortAsc(input);
         assertThat(input, is(expect));
     }
 
     @Test
     public void whenSortDesc() {
-        List<String> input = Arrays.asList("K2/SK1/SSK1", "K2", "K2/SK1", "K2/SK1/SSK2", "K1/SK1/SSK2"
-                , "K1", "K1/SK1", "K1/SK1/SSK1", "K1/SK2");
-        List<String> expect = Arrays.asList("K2", "K2/SK1", "K2/SK1/SSK1", "K2/SK1/SSK2", "K1", "K1/SK1"
-                , "K1/SK1/SSK1", "K1/SK1/SSK2", "K1/SK2");
-        Departments.sortAsc(input);
+        List<String> input = Departments.fillGaps(Arrays.asList(
+                "K2/SK1/SSK1"
+                , "K2"
+                , "K2/SK1"
+                , "K2/SK1/SSK2"
+                , "K1/SK1/SSK2"
+                , "K1"
+                , "K1/SK1"
+                , "K1/SK1/SSK1"
+                , "K1/SK2"));
+        List<String> expect = Arrays.asList(
+                "K2"
+                , "K2/SK1"
+                , "K2/SK1/SSK1"
+                , "K2/SK1/SSK2"
+                , "K1"
+                , "K1/SK1"
+                , "K1/SK1/SSK1"
+                , "K1/SK1/SSK2"
+                , "K1/SK2");
+        Departments.sortDesc(input);
         assertThat(input, is(expect));
     }
 }
