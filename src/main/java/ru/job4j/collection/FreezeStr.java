c7bd151526badc78d8641b9e1d4fdc4ea1272c7e
Scruner <aoveden1977@mail.ru>
2021-05-31 20:30:49 +0300

    2. Проверить две строчки на идентичность.

--- src/main/java/ru/job4j/collection/FreezeStr.java
+++ src/main/java/ru/job4j/collection/FreezeStr.java
@@ -5,15 +5,42 @@ import java.util.HashMap;
 public class FreezeStr {
 
     public static boolean eq(String left, String right) {
-        boolean rsl = true;
-        HashMap<String, String> hashMap = new HashMap<>();
-        for (String key : hashMap.keySet()) {
-            String value = hashMap.get(key);
-            if (!hashMap.containsValue(value)) {
-                rsl = false;
-                break;
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        int count = 1;
+        if (left.length() != right.length()) {
+            return false;
+        }
+        for (Character c : left.toCharArray()) {
+            if (!hashMap.containsKey(c)) {
+                hashMap.put(c, 1);
+            } else {
+                hashMap.put(c, hashMap.get(c) + 1);
             }
         }
-        return rsl;
+        for (Character c : right.toCharArray()) {
+            if (!hashMap.containsKey(c)) {
+                return false;
+            } else {
+                hashMap.put(c, hashMap.get(c) - 1);
+            }
+        }
+        for (Character c : hashMap.keySet()) {
+            if (hashMap.get(c) != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
+//        if (left.length() != right.length()) {
+//            return false;
+//        }
+//        char[] a = left.toCharArray();
+//        char[] b = right.toCharArray();
+//
+//        Arrays.sort(a);
+//        Arrays.sort(b);
+//        return Arrays.equals(a, b);
+//    }
+//
+
