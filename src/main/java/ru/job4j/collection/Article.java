534e301c25fb3b0d449fbaf9f68273b6897590a7
Scruner <aoveden1977@mail.ru>
2021-05-31 10:21:14 +0300

    1. Генератор текста

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -1,15 +1,23 @@
 package ru.job4j.collection;
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class Article {
 
     public static boolean generateBy(String origin, String line) {
-        String[] arrayLine = line.split(" ");
-        HashSet<String> hashSet = new HashSet<>();
-        for (String str : arrayLine) {
-            hashSet.add(str);
+        boolean rsl = true;
+        String[] arrayOrigin = origin.replaceAll("\\p{P}", "").split(" ");
+        String[] arrayLine = line.replaceAll("\\p{P}", "").split(" ");
+        HashSet<String> hashSet = new HashSet<>(Arrays.asList(arrayOrigin));
+        for (String lne : arrayLine) {
+            if (!hashSet.contains(lne)) {
+                rsl = false;
+                break;
+            }
         }
-        return origin.contains((CharSequence) hashSet);
+        return rsl;
     }
 }
+
+
