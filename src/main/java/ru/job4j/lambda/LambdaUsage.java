3630c51a1bb44f53aeb63711a209f71ee0906aaf
Scruner <aoveden1977@mail.ru>
2021-06-01 09:02:36 +0300

    2.2. Лямбда блок [#249208 #162744]

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -1,13 +1,20 @@
 package ru.job4j.lambda;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class LambdaUsage {
 
     public static void main(String[] args) {
+        String[] numbers = {
+                "1. Task",
+                "2. Task",
+                "11. Task"
+        };
         Comparator<String> cmpDescSize = (left, right) -> {
             System.out.println("compare - " + left.length() + " : " + right.length());
             return Integer.compare(right.length(), left.length());
         };
+        Arrays.sort(numbers, cmpDescSize);
     }
 }
