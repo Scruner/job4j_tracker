1f9e56a2eb67119f574fb1b3e25d32e1470e8fb4
Scruner <aoveden1977@mail.ru>
2021-06-02 13:17:24 +0300

    Tests fix error

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class PhoneDictionary {
 
-    private ArrayList<Person> persons = new ArrayList<Person>();
+    private final ArrayList<Person> persons = new ArrayList<Person>();
 
     public void add(Person person) {
         this.persons.add(person);
@@ -13,18 +13,18 @@ public class PhoneDictionary {
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
 
-//        for (Person searchPerson : persons) {
-//            if (searchPerson.getName()
-//                    .contains(key)
-//                    || searchPerson.getSurname()
-//                    .contains(key)
-//                    || searchPerson.getPhone()
-//                    .contains(key)
-//                    || searchPerson.getAddress()
-//                    .contains(key)) {
-//                result.add(searchPerson);
-//            }
-//        }
+        for (Person searchPerson : persons) {
+            if (searchPerson.getName()
+                    .contains(key)
+                    || searchPerson.getSurname()
+                    .contains(key)
+                    || searchPerson.getPhone()
+                    .contains(key)
+                    || searchPerson.getAddress()
+                    .contains(key)) {
+                result.add(searchPerson);
+            }
+        }
         return result;
     }
 }
