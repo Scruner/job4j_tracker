4127ca684d839fce725049f5b37439a6a81b450e
Scruner <aoveden1977@mail.ru>
2021-06-03 23:13:40 +0300

    0.1. Отладка лямбда в IDEA [#259165]

--- src/main/java/ru/job4j/stream/StreamUsage.java
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -1,22 +1,29 @@
 package ru.job4j.stream;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class StreamUsage {
 
+    public static class Task {
+        private final String name;
+        private final long spent;
+
+        public Task(String name, long spent) {
+            this.name = name;
+            this.spent = spent;
+        }
+    }
+
     public static void main(String[] args) {
-        List<Integer> arrayList = new ArrayList<>();
-        arrayList.add(3);
-        arrayList.add(-4);
-        arrayList.add(56);
-        arrayList.add(-97);
-        arrayList.add(-2);
-        arrayList.add(33);
-        arrayList.add(27);
-        List<Integer> positiveNum = arrayList.stream().filter(
-                arr -> arr > 0).collect(Collectors.toList());
-        positiveNum.forEach(System.out::println);
+        List<Task> tasks = List.of(
+                new Task("Bug #1", 10),
+                new Task("Task #2", 20),
+                new Task("Bug #3", 40)
+        );
+        tasks.stream()
+                .filter(task -> task.name.contains("Bug"))
+                .filter(task -> task.spent > 30)
+                .map(task -> task.name + " " + task.spent)
+                .forEach(System.out::println);
     }
 }
---
+++ src/main/java/ru/job4j/stream/StreamUsageInteg.java
@@ -0,0 +1,23 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamUsageInteg {
+
+    public static void main(String[] args) {
+        List<Integer> arrayList = new ArrayList<>();
+        arrayList.add(3);
+        arrayList.add(-4);
+        arrayList.add(56);
+        arrayList.add(-97);
+        arrayList.add(-2);
+        arrayList.add(33);
+        arrayList.add(27);
+        List<Integer> positiveNum = arrayList.stream()
+                .filter(arr -> arr > 0)
+                .collect(Collectors.toList());
+        positiveNum.forEach(System.out::println);
+    }
+}
