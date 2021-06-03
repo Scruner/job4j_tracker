f83d3efaaed1b9a293b568fcf33d9695106a4b04
Scruner <aoveden1977@mail.ru>
2021-06-03 23:49:36 +0300

    0. Stream API [#58166 #164358]

--- src/main/java/ru/job4j/stream/StreamUsageInteg.java
+++ src/main/java/ru/job4j/stream/StreamUsageInteg.java
@@ -1,20 +1,14 @@
 package ru.job4j.stream;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamUsageInteg {
 
     public static void main(String[] args) {
-        List<Integer> arrayList = new ArrayList<>();
-        arrayList.add(3);
-        arrayList.add(-4);
-        arrayList.add(56);
-        arrayList.add(-97);
-        arrayList.add(-2);
-        arrayList.add(33);
-        arrayList.add(27);
+        List<Integer> arrayList = new ArrayList<>(Arrays.asList(3, -4, 56, -97, -2, 33, 27));
         List<Integer> positiveNum = arrayList.stream()
                 .filter(arr -> arr > 0)
                 .collect(Collectors.toList());
