528fc0e70812548f734ae41e51aa4ab8b5cc6651
Scruner <aoveden1977@mail.ru>
2021-06-04 17:50:14 +0300

    1. Фильтрация учеников [#110058 #164494]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -1,16 +1,13 @@
 package ru.job4j.stream;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class School {
 
-    List<Student> list = new ArrayList<>();
-
     public List<Student> collect(List<Student> students, Predicate<Student> predict) {
-        return list.stream()
+        return students.stream()
                 .filter(predict)
                 .collect(Collectors.toList());
 
