76740bcc1c0e113cbb01df4f3c5e584e741b8c74
Scruner <aoveden1977@mail.ru>
2021-06-08 15:59:59 +0300

    4. Optional в Stream API. [#279130 #165790]

--- src/test/java/ru/job4j/bank/BankServiceTest.java
+++ src/test/java/ru/job4j/bank/BankServiceTest.java
@@ -11,19 +11,19 @@ public class BankServiceTest {
 
     @Test
     public void addUser() {
-        Optional<User> user = Optional.of(new User("3434", "Petr Arsentev"));
-        Optional<BankService> bank = Optional.of(new BankService());
-        bank.get().addUser(user.get());
-        assertThat(bank.get().findByPassport("3434"), is(user.get()));
+        User user = new User("3434", "Petr Arsentev");
+        BankService bank = new BankService();
+        bank.addUser(user);
+        assertThat(bank.findByPassport("3434").get(), is(user));
     }
 
     @Test
     public void whenEnterInvalidPassport() {
-        Optional<User> user = Optional.of(new User("3434", "Petr Arsentev"));
+        User user = new User("3434", "Petr Arsentev");
         BankService bank = new BankService();
-        bank.addUser(user.get());
-        bank.addAccount(user.get().getPassport(), new Account("5546", 150D));
-        assertThat(bank.findByRequisite(Optional.empty().get()));
+        bank.addUser(user);
+        bank.addAccount(user.getPassport(), new Account("5546", 150D));
+        assertThat(bank.findByRequisite("34", "5546"), is(Optional.empty()));
     }
 
     @Test
@@ -32,7 +32,8 @@ public class BankServiceTest {
         BankService bank = new BankService();
         bank.addUser(user);
         bank.addAccount(user.getPassport(), new Account("5546", 150D));
-        assertThat(bank.findByRequisite("3434", "5546").get().getBalance(), is(150D));
+        assertThat(bank.findByRequisite("3434", "5546").get().getBalance(),
+                   is(150D));
     }
 
     @Test
@@ -42,8 +43,11 @@ public class BankServiceTest {
         bank.addUser(user);
         bank.addAccount(user.getPassport(), new Account("5546", 150D));
         bank.addAccount(user.getPassport(), new Account("113", 50D));
-        bank.transferMoney(user.getPassport(), "5546", user.getPassport(), "113", 150D);
-        assertThat(bank.findByRequisite(user.getPassport(), "113").get().getBalance(), is(200D));
+        bank.transferMoney(user.getPassport(), "5546", user.getPassport(),
+                           "113", 150D);
+        assertThat(bank.findByRequisite(user.getPassport(), "113")
+                           .get()
+                           .getBalance(), is(200D));
     }
 
     @Test
@@ -53,6 +57,7 @@ public class BankServiceTest {
         bank.addUser(user);
         bank.addAccount(user.getPassport(), new Account("1111", 150D));
         bank.addAccount(user.getPassport(), new Account("1122", 300D));
-        assertThat(bank.findByRequisite("3434", "1122").get().getBalance(), is(300D));
+        assertThat(bank.findByRequisite("3434", "1122").get().getBalance(),
+                   is(300D));
     }
 }
